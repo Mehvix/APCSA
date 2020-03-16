2bf4e14d795c524a546a0bbdab62eb50f20963d4
MehvixLaptop <mehvix@protonmamil.com>
2020-03-16 14:33:42 -0500

    updated binary + other algos

--- src/Unit2_Arrays/searchSort.java
+++ src/Unit2_Arrays/searchSort.java
@@ -11,12 +11,13 @@ import java.util.*;
 
 public class searchSort {
     private static int[] generateDeck(int deck_size) {
+        // deck_size ++;  // extends so lst will have range of 0 through N
         int[] deck = new int[deck_size];
 
         for(int i = 0; i < deck_size; i++) {  // make the list not just zeros
             deck[i] += i;
         }
-        // shuffleDeck(deck);
+        shuffleDeck(deck);
         return deck;
     }
 
@@ -40,45 +41,108 @@ public class searchSort {
     }
 
     private static int binary_search(int target, int[] deck) {
-        int index_value = -1;
-        int index = (deck.length + 1)/(2);
-        int change = index;
-        while (index_value != target){
-            index_value = deck[index];
-            System.out.println("Looking at " + index_value + "...");
-            if (index_value == target){
-                System.out.println("FOUND!");
-                return index;
+        int bottom = 0, top = deck.length, middle;
+        while (bottom + 1 < deck.length){
+            middle = (bottom + top)/2;
+            System.out.println("Looking at " + middle + "...");
+            if(deck[middle] == target) {System.out.println("FOUND"); return middle;}
+            else if(deck[middle] > target) {System.out.println("Too high"); top = middle;}
+            else if(deck[middle] < target) {System.out.println("Too low"); bottom = middle;}
+        }
+        if(deck[deck.length - 1] == target){return (deck.length-1);}  // will never reach max because divide floors
+        else {return -1;}
+    }
+
+    private static boolean verify_sort(int[] deck) {
+        int previous = deck[0];
+        for (int i = 1; i < deck.length; i++) {
+            if (previous > deck[i]){
+                return false;
             }
-            else {
-                change = (change+1)/2;
-                if (index_value > target) {
-                    index -= change;
-                    System.out.println("Too high!");
-                } else {
-                    index += change;
-                    System.out.println("Too low!");
+        }
+        return true;
+    }
+
+    private static int[] bogo(int[] deck){
+        int count = 0;
+        while (!verify_sort(deck)){
+            shuffleDeck(deck);
+            count ++;
+        }
+        System.out.println("Bogo took " + count);
+        return deck;
+    }
+
+    private static int[] selection(int[] deck) {
+        int smallest, smallIndex, count = 0;
+        for(int i = 0; i<deck.length; i++){
+            //leftmost is smallest
+            smallest = deck[i];
+            smallIndex = i;
+
+            //look through rest of lst
+            for(int j = i+1; j < deck.length; j ++){
+                if(smallest > deck[j]){
+                    smallIndex = j;
+                    smallest = deck[j];
                 }
+                count ++;
             }
-            if (change == 1){
-                index_value = deck[index];
-                System.out.println("Looking at " + index_value + "...");
-                if (index_value == target) {
-                    System.out.println("FOUND!");
-                } else {
-                    System.out.println("Item is not in list!");
-                    return -1;
+
+            deck = swap(deck, i, smallIndex);
+        }
+        System.out.println("selection took " + count);
+        return deck;
+    }
+
+    private static int[] insertion(int[] deck) {
+        // todo
+        return deck;
+    }
+
+    private static int[] merge(int[] deck) {
+        // todo
+        return deck;
+    }
+
+    private static int[] bubble(int[] deck) {
+        int count = 0;
+        for(int i = 0; i < deck.length; i ++){
+            for(int j = 1; j < deck.length-i; j++) {
+                if(deck[j-1] > deck[j]){
+                    deck = swap(deck, j-1, j);
+                    count ++;
                 }
             }
         }
+        System.out.println("bubble took " + count);
+        return deck;
+    }
 
-        return index;
+    private static int[] bubbleBackwards(int[] deck) {
+        int count = 0;
+        for(int i = deck.length; i > 0; i --){
+            for(int j = 1; j < i; j++) {
+                if(deck[j-1] < deck[j]){
+                    deck = swap(deck, j-1, j);
+                    count ++;
+                }
+            }
+        }
+        System.out.println("bubble back took " + count);
+        return deck;
+    }
+
+    private static int[] swap(int[] deck, int index1, int index2) {
+        int[] tmp = deck.clone();  // this way tmp doesn't reference the deck's memory (because int[] a reference var, not a single value)
+        tmp[index1] = deck[index2];
+        tmp[index2] = deck[index1];
+        return tmp;
     }
 
     public static void main(String[] args) {
-        final int CARDS = 8000;
+        final int CARDS = 4;
         int[] deck = generateDeck(CARDS);
-        System.out.println(array1.convert(deck));
 
         /*
         Scanner sc = new Scanner(System.in);
@@ -86,8 +150,24 @@ public class searchSort {
         int target = sc.nextInt();
 
         System.out.println(find(target, deck));
+        System.out.println(binary_search(0, deck));
+        System.out.println(array1.convert(swap(deck, 0, 3)));
          */
 
-        System.out.println(binary_search(445, deck));
+        System.out.println("---\nBubble\nOriginal: " + array1.convert(deck));
+        System.out.println(array1.convert(bubble(deck)));
+
+        System.out.println("---\nBubble Back\nOriginal: " + array1.convert(deck));
+        System.out.println(array1.convert(bubbleBackwards(deck)));
+
+        shuffleDeck(deck);
+        System.out.println("---\nSelection\nOriginal: " + array1.convert(deck));
+
+        System.out.println(array1.convert(selection(deck)));
+
+        shuffleDeck(deck);
+        System.out.println("---\nBogo\nOriginal: " + array1.convert(deck));
+
+        System.out.println(array1.convert(bogo(deck)));
     }
 }
